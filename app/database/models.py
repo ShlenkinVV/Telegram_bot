716a256cf3f80985cfdba8941598913eb7ecb317
ShlenkinVV <mr.shlenkin@mail.ru>
2024-12-02 22:42:59 +0400

    fix env

--- app/database/models.py
+++ app/database/models.py
@@ -6,9 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
+DB_USER=os.getenv('DB_USER')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+DB_NAME=os.getenv('DB_NAME')
+DB_HOST=os.getenv('DB_HOST')
+DB_PORT=os.getenv('DB_PORT')
+
 #engine = create_async_engine(url=os.getenv('BD_URL3')) # создание БД
-engine = create_async_engine(url=os.getenv('BD_URL')) # создание БД
-#engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3') # создание БД
+engine = create_async_engine(url=f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}') # создание БД
+
 
 async_session = async_sessionmaker(engine) # Подкючение к БД
 
