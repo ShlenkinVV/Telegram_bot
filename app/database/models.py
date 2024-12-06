02d8577fcdcfaeabce6df33d331e2943afa8b4b3
ShlenkinVV <mr.shlenkin@mail.ru>
2024-12-06 21:15:13 +0400

    delete comments

--- app/database/models.py
+++ app/database/models.py
@@ -12,7 +12,7 @@ DB_NAME=os.getenv('DB_NAME')
 DB_HOST=os.getenv('DB_HOST')
 DB_PORT=os.getenv('DB_PORT')
 
-#engine = create_async_engine(url=os.getenv('BD_URL3')) # создание БД
+
 engine = create_async_engine(url=f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}') # создание БД
 
 
